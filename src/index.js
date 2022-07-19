79049cd0f252e717625e0410880dbd492595f6dd
Maria Makliuk <annamary_1995@mail.ru>
2022-07-19 13:33:12 +0300

    feat:finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = 
+  function check(str, configs) {
+    const rxArr = configs.map(getPairRx);
+    let testString = str;
+    let checkedNumber;
+    
+    do {
+      checkedNumber = testString.length;
+      testString = removeAllPairs(rxArr, testString);
+    } while(testString.length > 0 && testString.length !== checkedNumber);
+    
+    function escapeChars(string) {
+      return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
+    };
+  
+    function getPairRx(config) {
+      return new RegExp(`${escapeChars(config.join(''))}`, 'g');
+    }
+    
+    function removePair(rx, str) {
+      return str.replace(rx, '');
+    }
+    
+    function removeAllPairs(rxArr, str) {
+      rxArr.forEach(rx => { str = removePair(rx, str); });
+  
+      return str;
+    }
+  
+    return testString.length === 0;
+  }
+  
